af94682ca56dbee5409f7df921eee3913ff85866
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2015-04-22 15:46:31 -0400

    start on upstart initctl interface

--- daeman/initctl.py
+++ daeman/initctl.py
@@ -2,8 +2,77 @@
 Interaction with Upstart ``initctl`` command
 """
 
+from pyshc.sh import Sh
+
+initctl_command = Sh('initctl')
+
 
 class ManagerAPI(object):
 
     def status(self):
         raise NotImplementedError
+
+
+class StatusResult(object):
+    "The raw output of running ``initctl status <service>``"
+
+    def __init__(self, vals=None):
+        """
+        :param vals: the parsed output of ``initctl status``
+        """
+        self._vals = vals or dict()
+
+    @classmethod
+    def from_initctl_output(cls, string):
+        """Parse the output from ``systemctl status <name>``
+
+        :param string: the output string
+        :returns: the status result
+        :rtype: instance of :class:`StatusResult`
+        """
+        vals = dict()
+
+        # split on whitespace
+        words = string.strip().split()
+
+        # first element is the name of the service
+        name, words = words[0], words[1:]
+        vals['name'] = name
+
+        # the target goal and status towards goal
+        state, words = words[0], words[1:]
+        goal, state = state.rstrip(',').split('/')
+        vals['goal'] = goal
+        vals['state'] = state
+
+        # get PID if running
+        if goal == 'start' and state == 'running':
+            pid_str = words[-1]
+            vals['process'] = int(pid_str)
+
+        return cls(vals=vals)
+
+    @property
+    def name(self):
+        "The name of the service"
+        return self._vals['name']
+
+    @property
+    def goal(self):
+        "The target goal of the process"
+        return self._vals['goal']
+
+    @property
+    def state(self):
+        "The state of the services towards the goal"
+        return self._vals['state']
+
+    @property
+    def running(self):
+        "Is the service running?"
+        return 'process' in self._vals
+
+    @property
+    def process(self):
+        "The process ID of the service"
+        return self._vals['process']
--- tests/initctl_test.py
+++ tests/initctl_test.py
@@ -1,4 +1,4 @@
-from daeman.initctl import ManagerAPI
+from daeman.initctl import initctl_command, ManagerAPI, StatusResult
 from unittest import TestCase
 from inspect import getargspec
 from nose.plugins.attrib import attr
@@ -36,3 +36,36 @@ class TestManagerAPI_status(CheckManagerAPI, TestCase):
         self.argnames = args[1:]
         self.assertIsNone(defaults)
         self.defaults = list()
+
+
+@attr(service='upstart')
+class TestStatusResult(TestCase):
+
+    def setUp(self):
+        self.output_ssh = initctl_command('status ssh')
+        self.output_procps = initctl_command('status procps')
+
+    def test_running_service(self):
+        "Ensure that a running service is parsed"
+        StatusResult.from_initctl_output(self.output_ssh)
+
+    def test_stopped_service(self):
+        "Ensure that a stopped service is parsed"
+        StatusResult.from_initctl_output(self.output_procps)
+
+    def test_running_service_correct(self):
+        "Ensure that a running service is parsed correctly"
+        status = StatusResult.from_initctl_output(self.output_ssh)
+        self.assertEqual(status.name, 'ssh')
+        self.assertEqual(status.goal, 'start')
+        self.assertEqual(status.state, 'running')
+        self.assertIsInstance(status.process, int)
+        self.assertGreater(status.process, 0)
+
+    def test_stopped_service_correct(self):
+        "Ensure that a stopped service is parsed correctly"
+        status = StatusResult.from_initctl_output(self.output_procps)
+        self.assertEqual(status.name, 'procps')
+        self.assertEqual(status.goal, 'stop')
+        self.assertEqual(status.state, 'waiting')
+        self.assertNotIn('process', status._vals)
